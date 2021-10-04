26bd4302167e6b0fb33caad98e2520cf436b590e
Zohaib <zohaibmanzoor1700@gmail.com>
2021-10-04 14:22:34 +0000

    5/6 completed

--- index.js
+++ index.js
@@ -1,25 +1,49 @@
 //1
-function countToTen(){
-  
-}
+const countFromOne = (int) => {
+    for (let i = 1; i <= int; i++){
+    console.log(i)
+    }
+};
+//countFromOne(10)
 
 //2
-function countFromOne(){
-  
-}
+
+const countEveryOdd = (int) => {
+    for (let i = 1; i <= int; i+= 2){
+    console.log(i)
+    }
+};
+//countEveryOdd(20)
 
 //3
-function countEveryEven(){
-  
+const isNegative = (num) => {
+    if (num > 0 ){
+    return false;
+    } else {
+    return true;
+  }
 }
+
+//console.log(isNegative(-30))
 
 //4
-function countEveryOdd(){
-  
+const betweenFiveAndTwenty = (int) => {
+    if (int >= 5 && int <= 20){
+        return true;
+    } else {
+        return false;
+    }
 }
+//console.log(betweenFiveAndTwenty(30))
 
-//5 
-function countEvens(){
-  
-}
+//5  
+function isAllLowerCase (string){
+    const lowercase = string.toLowerCase();
+    if(lowercase === string){
+        return true
+        } else {
+            return false;
+        }
+    }
 
+console.log('Hello')
